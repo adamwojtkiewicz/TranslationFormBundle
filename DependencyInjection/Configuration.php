70f8043849e17acb4d9a1a8972696bb64f18e80b
adamwojtkiewicz <adam.wojtkiewicz@gmail.com>
2024-09-02 15:57:32 +0200

    Update Configuration.php
    Removed deprecation

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,7 +23,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('a2lix_translation_form');
 
